6c28178e92095dcc1755cf73eb14e6dedc8bed86
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-06-20 23:45:20 +0300

    add and delete doctor

--- ICareClient/src/store/actions/doctors/doctorsActions.js
+++ ICareClient/src/store/actions/doctors/doctorsActions.js
@@ -1,15 +1,44 @@
-import { APIS, HTTP_REQUEST } from "constants/constants";
-import { DOCTOR_GET_DOCTORS_LIST } from "./doctorsActionTypes";
+import {APIS} from "constants/constants";
+import {
+    DOCTOR_GET_DOCTORS_LIST,
+    DOCTOR_ADD_DOCTOR,
+    DOCTOR_DELETE_DOCTOR
+} from "./doctorsActionTypes";
 
-const { apiCaller } = require("store/actions/app/appActions");
+const {apiCaller} = require("store/actions/app/appActions");
 
 export function callGetDoctors(onStart, onSuccess, onFailure) {
-  return apiCaller({
-    url: APIS.doctors,
-    method: HTTP_REQUEST.GET,
-    actionType: DOCTOR_GET_DOCTORS_LIST,
-    onStart,
-    onSuccess,
-    onFailure
-  });
+    return apiCaller({
+        url: APIS.doctors.getDoctors.url,
+        method: APIS.doctors.getDoctorById.method,
+        actionType: DOCTOR_GET_DOCTORS_LIST,
+        onStart,
+        onSuccess,
+        onFailure
+    });
+}
+
+export function callAddDoctor(data, onStart, onSuccess, onFailure) {
+    const api = APIS.doctors.addDoctor;
+    return apiCaller({
+        data,
+        url: api.url,
+        method: api.method,
+        actionType: DOCTOR_ADD_DOCTOR,
+        onStart,
+        onSuccess,
+        onFailure
+    });
+}
+
+export function callDeleteDoctor(id, onStart, onSuccess, onFailure) {
+    const api = APIS.doctors.deleteDoctor;
+    return apiCaller({
+        url: api.url.replace("{id}", id),
+        method: api.method,
+        actionType: DOCTOR_DELETE_DOCTOR,
+        onStart,
+        onSuccess,
+        onFailure
+    });
 }
--- ICareClient/src/store/reducers/doctorsReducer.js
+++ ICareClient/src/store/reducers/doctorsReducer.js
@@ -1,26 +1,42 @@
-import { PURGE } from "store/actions/app/appActionTypes";
-import { DOCTOR_GET_DOCTORS_LIST_SUCCESS } from "store/actions/doctors/doctorsActionTypes";
+import {PURGE} from "store/actions/app/appActionTypes";
+import {
+    DOCTOR_GET_DOCTORS_LIST_SUCCESS,
+    DOCTOR_ADD_DOCTOR_SUCCESS,
+    DOCTOR_DELETE_DOCTOR_SUCCESS
+} from "store/actions/doctors/doctorsActionTypes";
 
 const initialState = {
-  doctorsList: []
+    doctorsList: []
 };
-const doctorsReducer = (state = initialState, { data, type, id }) => {
-  let updatedDoctorsList = [...state.doctorsList];
+const doctorsReducer = (state = initialState, {data, type, id}) => {
+    let updatedDoctorsList = [...state.doctorsList];
 
-  switch (type) {
-    case DOCTOR_GET_DOCTORS_LIST_SUCCESS:
-      updatedDoctorsList = data;
-      return {
-        ...state,
-        doctorsList: updatedDoctorsList
-      };
-    case PURGE:
-      return {
-        ...initialState
-      };
-    default:
-      return state;
-  }
+    switch (type) {
+        case DOCTOR_GET_DOCTORS_LIST_SUCCESS:
+            updatedDoctorsList = data;
+            return {
+                ...state,
+                doctorsList: updatedDoctorsList
+            };
+
+        case DOCTOR_ADD_DOCTOR_SUCCESS:
+            updatedDoctorsList.push(data);
+            return {
+                ...state,
+                doctorsList: updatedDoctorsList
+            };
+        case DOCTOR_DELETE_DOCTOR_SUCCESS:
+            return {
+                ...state,
+                doctorsList: updatedDoctorsList.fill()
+            };
+        case PURGE:
+            return {
+                ...initialState
+            };
+        default:
+            return state;
+    }
 };
 
 export default doctorsReducer;
