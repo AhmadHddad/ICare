cf75b7c92ecc75e50a5341a2c9a67ddd064c949c
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-06-20 23:43:57 +0300

    removeMargin

--- ICareClient/src/shared/GeneralStyles.jsx
+++ ICareClient/src/shared/GeneralStyles.jsx
@@ -1,25 +1,28 @@
-const GeneralStyles = theme => ({
-  disabled: {
-    "pointer-events": "none",
-    "-webkit-user-select": "none",
-    "-moz-user-select": "none",
-    "-ms-user-select": "none",
-    "user-select": "none",
-    opacity: 0.45,
-  },
-  textAlignCenter: {
-    textAlign: "center",
-  },
-  cursorPointer: {
-    cursor: "pointer",
-  },
-  verticalAlignMiddle: {
-    verticalAlign: "middle",
-  },
-  removeSidePadding: {
-    paddingLeft: "0 !important",
-    paddingRight: "0 !important",
-  },
+const GeneralStyles = (theme) => ({
+    disabled: {
+        "pointer-events": "none",
+        "-webkit-user-select": "none",
+        "-moz-user-select": "none",
+        "-ms-user-select": "none",
+        "user-select": "none",
+        opacity: 0.45
+    },
+    textAlignCenter: {
+        textAlign: "center"
+    },
+    cursorPointer: {
+        cursor: "pointer"
+    },
+    verticalAlignMiddle: {
+        verticalAlign: "middle"
+    },
+    removeSidePadding: {
+        paddingLeft: "0 !important",
+        paddingRight: "0 !important"
+    },
+    removeMargin: {
+        margin: "0 !important"
+    }
 });
 
 export default GeneralStyles;
