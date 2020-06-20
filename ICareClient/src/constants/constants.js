49d94dfcf0f22b4bd78f58b34c787c3a494cbac4
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-06-20 23:47:17 +0300

    adding new constants

--- ICareClient/src/constants/constants.js
+++ ICareClient/src/constants/constants.js
@@ -1,36 +1,42 @@
 export const REGEX = {
-  email: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
-  number: /^[0-9][A-Za-z0-9 -]*$/
-};
-
-export const APIS = {
-  login: "auth/login",
-  register: "auth/register",
-  patients: "patients",
-  records: "records",
-  statistics: "patients/statistics",
-  doctors: "doctors"
-};
-
-export const MESSAGE_TYPES = {
-  error: "error",
-  success: "success"
+    email: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
+    number: /^[0-9][A-Za-z0-9 -]*$/
 };
 
 export const HTTP_REQUEST = {
-  GET: "get",
-  POST: "post",
-  PUT: "put",
-  DELETE: "delete"
+    GET: "get",
+    POST: "post",
+    PUT: "put",
+    DELETE: "delete"
+};
+
+export const APIS = {
+    login: "auth/login",
+    register: "auth/register",
+    patients: "patients",
+    records: "records",
+    statistics: "patients/statistics",
+    doctors: {
+        getDoctors: {url: "doctors", method: HTTP_REQUEST.GET},
+        getDoctorById: {url: "doctors/{id}", method: HTTP_REQUEST.GET},
+        deleteDoctor: {url: "doctors/{id}", method: HTTP_REQUEST.DELETE},
+        addDoctor: {url: "doctors", method: HTTP_REQUEST.POST},
+        assignPatient: {url: "doctors/{doctorId}/{patientId}", method: HTTP_REQUEST.POST}
+    }
+};
+
+export const MESSAGE_TYPES = {
+    error: "error",
+    success: "success"
 };
 
 export const PAGINATION = {
-  pageNumber: "pageNumber",
-  pageSize: "pageSize"
+    pageNumber: "pageNumber",
+    pageSize: "pageSize"
 };
 
 export const DEFAULT_PAGINATION_VALUES = {
-  currentPage: 1,
-  itemsPerPage: 5,
-  totalItems: 5
+    currentPage: 1,
+    itemsPerPage: 5,
+    totalItems: 5
 };
