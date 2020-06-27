b7381b3c0bc13b639556544db1f17448be1710f8
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-06-28 00:05:57 +0300

    renaming to PatientForEditDto

--- ICareAPI/Dtos/PatientForAddEditDto.cs
+++ ICareAPI/Dtos/PatientForAddEditDto.cs
@@ -3,19 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ICareAPI.Dtos
 {
-    public class PatientForAddEditDto
+    public class PatientForEditDto
     {
         public int Id { get; set; }
 
 
         [Required]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "User Name Minimum Length Is 3 And Maximum Is 20!")]
-        public string Name { get; set; }
-
-        [Required]
-        [StringLength(9, MinimumLength = 9, ErrorMessage = "OfficialId must be 9 digits!")]
-        public string OfficialId { get; set; }
-
+        public string Name { get; set; } = "";
 
         [Required]
         public DateTime DateOfBirth { get; set; }
