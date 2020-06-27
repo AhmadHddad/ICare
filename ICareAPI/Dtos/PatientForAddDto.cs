06b0494a6233fe0aea53fc0a9874542bb4d7e959
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-06-28 00:05:27 +0300

    PatientForAddDto

---
+++ ICareAPI/Dtos/PatientForAddDto.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ICareAPI.Dtos
+{
+    public class PatientForAddDto
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "User Name Minimum Length Is 3 And Maximum Is 20!")]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "OfficialId must be 9 digits!")]
+        public string OfficialId { get; set; } = "";
+
+
+        [Required]
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+
+        public string? Email { get; set; }
+
+    }
+}
