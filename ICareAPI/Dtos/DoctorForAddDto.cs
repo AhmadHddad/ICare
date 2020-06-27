b546c0f744c6a5974a472dae4ea373ebd3c73df1
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-06-28 00:05:41 +0300

    default values

--- ICareAPI/Dtos/DoctorForAddDto.cs
+++ ICareAPI/Dtos/DoctorForAddDto.cs
@@ -8,25 +8,25 @@ namespace ICareAPI.Dtos
         [Required]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "User Name Minimum Length Is 3 And Maximum Is 20!")]
 
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
 
         [Required]
         [StringLength(9, MinimumLength = 9, ErrorMessage = "OfficialId must be 9 digits!")]
-        public string OfficialId { get; set; }
+        public string OfficialId { get; set; } = "";
 
         [Required]
-        public string Specialty { get; set; }
+        public string Specialty { get; set; } = "";
 
         [Required]
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
 
         public string? Email { get; set; }
 
         [Required]
-        public string University { get; set; }
+        public string University { get; set; } = "";
 
         [Required]
-        public string Department { get; set; }
+        public string Department { get; set; } = "";
 
 
     }
