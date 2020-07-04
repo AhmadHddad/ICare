955e01f2ccf3220974234a71ea0404b7ea9f32bf
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-07-05 00:28:15 +0300

    buildMap

--- ICareClient/src/utils/utils.js
+++ ICareClient/src/utils/utils.js
@@ -1,18 +1,34 @@
 export const toggle = (name, stateObj, callback) => event => {
-  callback && callback(prev => ({ ...stateObj, [name]: !prev[name] }));
+    callback && callback(prev => ({...stateObj, [name]: !prev[name]}));
 };
 
 export function parseJwt(token) {
-  var base64Url = token.split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+        atob(base64)
+            .split("")
+            .map(function (c) {
+                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join("")
+    );
 
-  return JSON.parse(jsonPayload);
+    return JSON.parse(jsonPayload);
+}
+
+export function buildMap(obj = Object) {
+    return new Map(Object.entries(obj));
+}
+
+export function isLength(objOrArr) {
+    if (!objOrArr) return false;
+
+    if (Array.isArray(objOrArr)) {
+        return !!objOrArr.length;
+    }
+
+    if (typeof objOrArr === "object") {
+        return !!Object.keys(objOrArr).length;
+    }
 }
