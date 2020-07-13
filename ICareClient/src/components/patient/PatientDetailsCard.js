6cd27f814695e38c46e83b0c11b36e64656c2266
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-07-13 06:33:40 +0300

    format

--- ICareClient/src/components/patient/PatientDetailsCard.js
+++ ICareClient/src/components/patient/PatientDetailsCard.js
@@ -1,183 +1,161 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {
-  Grid,
-  TextField,
-  Button,
-  Card,
-  Typography,
-  CardContent,
-  makeStyles,
+    Grid,
+    TextField,
+    Button,
+    Card,
+    Typography,
+    CardContent,
+    makeStyles
 } from "@material-ui/core";
 
-import AssignmentIndRoundedIcon from "@material-ui/icons/AssignmentIndRounded";
-import { Skeleton } from "@material-ui/lab";
+import DetailsIcon from "@material-ui/icons/Details";
+import {Skeleton} from "@material-ui/lab";
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
+    root: {
+        minWidth: 275
+    },
 
-  patientDetailsText: {
-    marginBottom: 20,
-  },
-  icon: {
-    verticalAlign: "middle",
-    marginBottom: 3,
-    marginLeft: 5,
-  },
-  skeleton: {
-    borderRadius: 10,
-  },
+    patientDetailsText: {
+        marginBottom: 20
+    },
+    icon: {
+        verticalAlign: "middle",
+        marginBottom: 3,
+        marginLeft: 5
+    },
+    skeleton: {
+        borderRadius: 10
+    }
 });
 
-export default function PatientDetailsCard({
-  patientDetails,
-  onEdit,
-  isLoading,
-}) {
-  const classes = useStyles();
+export default function PatientDetailsCard({patientDetails, onEdit, isLoading}) {
+    const classes = useStyles();
 
-  const renderPatientDetailsFields = () => (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      spacing={2}
-    >
-      <Grid item xs={12} md={6} sm={6}>
-        <TextField
-          disabled
-          fullWidth
-          value={patientDetails.name}
-          id="outlined-basic"
-          label={"Patient Name"}
-          variant="standard"
-        />
-      </Grid>
-      <Grid item xs={12} md={6} sm={6}>
-        <TextField
-          disabled
-          fullWidth
-          value={new Date(patientDetails.dateOfBirth).toLocaleDateString()}
-          id="outlined-basic"
-          label={"Date Of Birth"}
-          variant="standard"
-        />
-      </Grid>
-      <Grid item xs={12} md={6} sm={6}>
-        <TextField
-          disabled
-          fullWidth
-          value={patientDetails.officialId}
-          id="outlined-basic"
-          label={"Official ID"}
-          variant="standard"
-        />
-      </Grid>
-      <Grid item xs={12} md={6} sm={6}>
-        <TextField
-          disabled
-          fullWidth
-          value={patientDetails.email}
-          id="outlined-basic"
-          label={"Email"}
-          variant="standard"
-        />
-      </Grid>
-    </Grid>
-  );
-
-  const renderSkeleton = () => (
-    <Grid
-      item
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      spacing={2}
-    >
-      <Grid item md={6} sm={6} xs={12}>
-        <Skeleton
-          className={classes.skeleton}
-          variant="rect"
-          width={"100%"}
-          height={72}
-          animation="wave"
-        />
-      </Grid>
-      <Grid item md={6} sm={6} xs={12}>
-        <Skeleton
-          className={classes.skeleton}
-          variant="rect"
-          width={"100%"}
-          height={72}
-          animation="wave"
-        />
-      </Grid>
-      <Grid item md={6} sm={6} xs={12}>
-        <Skeleton
-          className={classes.skeleton}
-          variant="rect"
-          width={"100%"}
-          height={72}
-          animation="wave"
-        />
-      </Grid>
-      <Grid item md={6} sm={6} xs={12}>
-        <Skeleton
-          className={classes.skeleton}
-          variant="rect"
-          width={"100%"}
-          height={72}
-          animation="wave"
-        />
-      </Grid>
-    </Grid>
-  );
-
-  return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="stretch"
-        >
-          <Grid item>
-            <Typography
-              className={classes.patientDetailsText}
-              variant="h5"
-              gutterBottom
-              component="h2"
-              color="textSecondary"
-            >
-              Patient Details
-              <AssignmentIndRoundedIcon className={classes.icon} />
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Button
-              disabled={isLoading}
-              size="small"
-              variant="contained"
-              color="primary"
-              onClick={onEdit}
-            >
-              Edit
-            </Button>
-          </Grid>
+    const renderPatientDetailsFields = () => (
+        <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
+            <Grid item xs={12} md={6} sm={6}>
+                <TextField
+                    disabled
+                    fullWidth
+                    value={patientDetails.name}
+                    id="outlined-basic"
+                    label={"Patient Name"}
+                    variant="standard"
+                />
+            </Grid>
+            <Grid item xs={12} md={6} sm={6}>
+                <TextField
+                    disabled
+                    fullWidth
+                    value={new Date(patientDetails.dateOfBirth).toLocaleDateString()}
+                    id="outlined-basic"
+                    label={"Date Of Birth"}
+                    variant="standard"
+                />
+            </Grid>
+            <Grid item xs={12} md={6} sm={6}>
+                <TextField
+                    disabled
+                    fullWidth
+                    value={patientDetails.officialId}
+                    id="outlined-basic"
+                    label={"Official ID"}
+                    variant="standard"
+                />
+            </Grid>
+            <Grid item xs={12} md={6} sm={6}>
+                <TextField
+                    disabled
+                    fullWidth
+                    value={patientDetails.email}
+                    id="outlined-basic"
+                    label={"Email"}
+                    variant="standard"
+                />
+            </Grid>
         </Grid>
-        {isLoading ? renderSkeleton() : renderPatientDetailsFields()}
-      </CardContent>
-    </Card>
-  );
+    );
+
+    const renderSkeleton = () => (
+        <Grid item container direction="row" justify="center" alignItems="center" spacing={2}>
+            <Grid item md={6} sm={6} xs={12}>
+                <Skeleton
+                    className={classes.skeleton}
+                    variant="rect"
+                    width={"100%"}
+                    height={72}
+                    animation="wave"
+                />
+            </Grid>
+            <Grid item md={6} sm={6} xs={12}>
+                <Skeleton
+                    className={classes.skeleton}
+                    variant="rect"
+                    width={"100%"}
+                    height={72}
+                    animation="wave"
+                />
+            </Grid>
+            <Grid item md={6} sm={6} xs={12}>
+                <Skeleton
+                    className={classes.skeleton}
+                    variant="rect"
+                    width={"100%"}
+                    height={72}
+                    animation="wave"
+                />
+            </Grid>
+            <Grid item md={6} sm={6} xs={12}>
+                <Skeleton
+                    className={classes.skeleton}
+                    variant="rect"
+                    width={"100%"}
+                    height={72}
+                    animation="wave"
+                />
+            </Grid>
+        </Grid>
+    );
+
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Grid container direction="row" justify="space-between" alignItems="stretch">
+                    <Grid item>
+                        <Typography
+                            className={classes.patientDetailsText}
+                            variant="h5"
+                            gutterBottom
+                            component="h2"
+                            color="textSecondary"
+                        >
+                            Patient Details
+                            <DetailsIcon className={classes.icon} />
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            disabled={isLoading}
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                            onClick={onEdit}
+                        >
+                            Edit
+                        </Button>
+                    </Grid>
+                </Grid>
+                {isLoading ? renderSkeleton() : renderPatientDetailsFields()}
+            </CardContent>
+        </Card>
+    );
 }
 
 PatientDetailsCard.propTypes = {
-  patientDetails: PropTypes.object,
-  onEdit: PropTypes.func,
-  isLoading: PropTypes.bool,
+    patientDetails: PropTypes.object,
+    onEdit: PropTypes.func,
+    isLoading: PropTypes.bool
 };
--- ICareClient/src/components/tableWithBtnLayout/TableWithBtnLayout.js
+++ ICareClient/src/components/tableWithBtnLayout/TableWithBtnLayout.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // Material UI
-import { Grid, Button, makeStyles } from "@material-ui/core";
+import {Grid, Button, makeStyles} from "@material-ui/core";
 
 // Icons
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
@@ -12,68 +12,66 @@ import GeneralStyles from "shared/GeneralStyles";
 import ITable from "components/table/ITable";
 
 const useStyles = makeStyles(theme => ({
-  ...GeneralStyles(),
-  root: {
-    marginRight: theme.spacing(1),
-    verticalAlign: "middle",
-    width: 16,
-    height: 16
-  },
-  tableContainer: {
-    marginTop: 20
-  }
+    ...GeneralStyles(),
+    root: {
+        marginRight: theme.spacing(1),
+        verticalAlign: "middle",
+        width: 16,
+        height: 16
+    },
+    tableContainer: {
+        marginTop: 20
+    }
 }));
 
 export default function TableWithBtnLayout({
-  addIcon,
-  onAddBtnClick,
-  AddBtnLabel,
-  tableDataRows,
-  isLoadingData,
-  tableHeaders,
-  pagination,
-  onChangePage,
-  onChangeRowsPerPage
+    addIcon,
+    onAddBtnClick,
+    AddBtnLabel,
+    tableDataRows,
+    isLoadingData,
+    tableHeaders,
+    pagination,
+    onChangePage,
+    onChangeRowsPerPage
 }) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Grid container direction="row" justify="center" alignItems="center">
-      <Grid item container md={12} xs={12} justify="flex-end">
-        <Grid item>
-          <Button onClick={onAddBtnClick} variant="contained" color="primary">
-            {addIcon || (
-              <PersonAddIcon fontSize="small" className={classes.root} />
-            )}
-            {AddBtnLabel}
-          </Button>
+    return (
+        <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item container md={12} xs={12} justify="flex-end">
+                <Grid item>
+                    <Button onClick={onAddBtnClick} variant="contained" color="primary">
+                        {addIcon || <PersonAddIcon fontSize="small" className={classes.root} />}
+                        {AddBtnLabel}
+                    </Button>
+                </Grid>
+            </Grid>
+            <Grid item md={12} xs={12} className={classes.tableContainer}>
+                <ITable
+                    onChangePage={onChangePage}
+                    onChangeRowsPerPage={onChangeRowsPerPage}
+                    page={pagination?.currentPage}
+                    rowsPerPage={pagination?.itemsPerPage}
+                    totalItems={pagination?.totalItems}
+                    addPagination
+                    isLoading={isLoadingData}
+                    rows={tableDataRows}
+                    headers={tableHeaders}
+                />
+            </Grid>
         </Grid>
-      </Grid>
-      <Grid item md={12} xs={12} className={classes.tableContainer}>
-        <ITable
-          onChangePage={onChangePage}
-          onChangeRowsPerPage={onChangeRowsPerPage}
-          page={pagination.currentPage}
-          rowsPerPage={pagination.itemsPerPage}
-          totalItems={pagination.totalItems}
-          addPagination
-          isLoading={isLoadingData}
-          rows={tableDataRows}
-          headers={tableHeaders}
-        />
-      </Grid>
-    </Grid>
-  );
+    );
 }
 
 TableWithBtnLayout.propTypes = {
-  addIcon: PropTypes.node,
-  onAddBtnClick: PropTypes.func,
-  AddBtnLabel: PropTypes.string,
-  tableDataRows: PropTypes.array,
-  isLoadingData: PropTypes.bool,
-  tableHeaders: PropTypes.array,
-  pagination: PropTypes.object,
-  onChangePage: PropTypes.func,
-  onChangeRowsPerPage: PropTypes.func
+    addIcon: PropTypes.node,
+    onAddBtnClick: PropTypes.func,
+    AddBtnLabel: PropTypes.string,
+    tableDataRows: PropTypes.array,
+    isLoadingData: PropTypes.bool,
+    tableHeaders: PropTypes.array,
+    pagination: PropTypes.object,
+    onChangePage: PropTypes.func,
+    onChangeRowsPerPage: PropTypes.func
 };
