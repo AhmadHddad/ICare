214dd6a019b693ff7580eac85fe26411d6fd77e6
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-07-13 06:32:45 +0300

    remmve unessary code

--- ICareClient/src/components/patient/PatientRecordsCardStyle.jsx
+++ ICareClient/src/components/patient/PatientRecordsCardStyle.jsx
@@ -1,34 +1,30 @@
 import GeneralStyles from "../../shared/GeneralStyles";
 const PatientRecordsCardStyle = theme => ({
-  ...GeneralStyles(),
-  root: {
-    minWidth: 275,
-  },
-  patientDetailsText: {
-    marginBottom: 20,
-  },
-  icon: {
-    verticalAlign: "middle",
-    marginBottom: 3,
-    marginLeft: 5,
-  },
-  tablePaper: {
-    boxShadow: "none",
-    border: "1px solid whitesmoke",
-  },
-  desc: {
-    fontSize: "unset",
-    maxWidth: "20vw",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap",
-  },
-  expandedDesc: {
-    padding: "5px 0",
-  },
-  editIcon: {
-    margin: "0 5px",
-  },
+    ...GeneralStyles(),
+    root: {
+        minWidth: 275
+    },
+    patientDetailsText: {
+        marginBottom: 20
+    },
+    icon: {
+        verticalAlign: "middle",
+        marginBottom: 3,
+        marginLeft: 5
+    },
+    desc: {
+        fontSize: "unset",
+        maxWidth: "20vw",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        whiteSpace: "nowrap"
+    },
+    expandedDesc: {
+        padding: "5px 0"
+    },
+    editIcon: {
+        margin: "0 5px"
+    }
 });
 
 export default PatientRecordsCardStyle;
