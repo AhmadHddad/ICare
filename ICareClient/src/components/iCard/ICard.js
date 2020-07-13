db8987f684500aca44eb5736b5ca56823213b81b
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-07-13 06:28:51 +0300

    adding Icard component

---
+++ ICareClient/src/components/iCard/ICard.js
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Grid, makeStyles, Card, CardContent, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275
+    },
+    patientDetailsText: {
+        marginBottom: 20
+    },
+    icon: {
+        verticalAlign: "middle",
+        marginBottom: 4,
+        marginLeft: 8
+    }
+});
+
+const propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.any,
+    footer: PropTypes.any,
+    icon: PropTypes.object,
+    justify: PropTypes.string,
+    alignItems: PropTypes.string,
+    header: PropTypes.any
+};
+
+const defaultProps = {
+    justify: "space-between",
+    alignItems: "stretch"
+};
+
+export default function ICard({title, children, footer, icon: Icon, justify, alignItems, header}) {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Grid container direction="row" justify={justify} alignItems={alignItems}>
+                    <Grid item>
+                        <Typography
+                            className={classes.patientDetailsText}
+                            variant="h5"
+                            gutterBottom
+                            component="h2"
+                            color="textSecondary"
+                        >
+                            {title}
+                            {Icon && <Icon className={classes.icon} />}
+                        </Typography>
+                    </Grid>
+                    {header}
+                </Grid>
+                {children}
+            </CardContent>
+        </Card>
+    );
+}
+
+ICard.propTypes = propTypes;
+ICard.defaultProps = defaultProps;
