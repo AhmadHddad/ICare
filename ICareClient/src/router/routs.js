764a9885c8c696e99b5c120e9c32ef44d7c49376
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-07-13 06:37:28 +0300

    adding Doctors details rout

--- ICareClient/src/router/routs.js
+++ ICareClient/src/router/routs.js
@@ -1,53 +1,51 @@
-import { lazy } from "react";
+import {lazy} from "react";
 
 // Icons
-import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
 import AirlineSeatFlatIcon from "@material-ui/icons/AirlineSeatFlat";
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
 
 // Lazy Imports
-const lazyPatientsList = lazy(() =>
-  import("containers/patients/patientsList/PatientsList")
-);
-const lazyPatientDetails = lazy(() =>
-  import("containers/patients/patientDetails/PatientDetails")
-);
-const lazyLoginAndRegister = lazy(() =>
-  import("containers/login/LoginAndRegister")
-);
-const lazyDoctorsList = lazy(_ =>
-  import("containers/doctors/doctorsList/DoctorsList")
-);
+const lazyPatientsList = lazy(() => import("containers/patients/patientsList/PatientsList"));
+const lazyPatientDetails = lazy(() => import("containers/patients/patientDetails/PatientDetails"));
+const lazyLoginAndRegister = lazy(() => import("containers/login/LoginAndRegister"));
+const lazyDoctorsList = lazy(_ => import("containers/doctors/doctorsList/DoctorsList"));
+const lazyDoctorDetails = lazy(_ => import("containers/doctors/doctorDetails/DoctorDetails"));
 
 const routs = [
-  {
-    id: 1,
-    name: "Patients list",
-    path: "/pationslist",
-    component: lazyPatientsList,
-    icon: AirlineSeatFlatIcon,
-    isMain: true
-  },
-  {
-    id: 2,
-    name: "Patients list",
-    path: "/pationslist/:id",
-    component: lazyPatientDetails,
-    icon: FormatListBulletedIcon,
-    hidden: true
-  },
-  {
-    id: 3,
-    name: "Doctors list",
-    path: "/doctorslist",
-    component: lazyDoctorsList,
-    icon: AssignmentIndIcon
-  }
+    {
+        id: 1,
+        name: "Patients list",
+        path: "/pationslist",
+        component: lazyPatientsList,
+        icon: AirlineSeatFlatIcon,
+        isMain: true
+    },
+    {
+        id: 2,
+        name: "Patients details",
+        path: "/pationslist/:id",
+        component: lazyPatientDetails,
+        hidden: true
+    },
+    {
+        id: 3,
+        name: "Doctors list",
+        path: "/doctorslist",
+        component: lazyDoctorsList,
+        icon: AssignmentIndIcon
+    },
+    {
+        id: 4,
+        name: "Doctors details",
+        path: "/doctorslist/:id",
+        component: lazyDoctorDetails,
+        hidden: true
+    }
 ];
 
 export const loginPage = {
-  name: "Login",
-  path: "/login",
-  component: lazyLoginAndRegister
+    name: "Login",
+    path: "/login",
+    component: lazyLoginAndRegister
 };
 export default routs;
