b7606f0a76711f59a02cf3b2f9d8367dc08aba49
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-07-13 06:29:53 +0300

    code enhancments

--- ICareClient/src/App.js
+++ ICareClient/src/App.js
@@ -1,115 +1,111 @@
-import React, { Suspense, useEffect, Fragment, useState } from "react";
-import { connect } from "react-redux";
-import { Route, Redirect, Switch, withRouter } from "react-router-dom";
+import React, {Suspense, useEffect, Fragment, useState, useCallback} from "react";
+import {connect} from "react-redux";
+import {Route, Redirect, Switch, withRouter} from "react-router-dom";
 
-import router, { loginPage } from "./router/routs";
+import router, {loginPage} from "./router/routs";
 
 import Layout from "./containers/layout/Layout.js";
 
-import { checkIfAuth } from "store/actions/auth/authActions.js";
+import {checkIfAuth} from "store/actions/auth/authActions.js";
 
-import { Snackbar } from "@material-ui/core";
+import {Snackbar} from "@material-ui/core";
 import MuiAlert from "@material-ui/lab/Alert";
-import { deleteMessage, purgeApp } from "store/actions/app/appActions";
+import {deleteMessage, purgeApp} from "store/actions/app/appActions";
 
-function App({ dispatch, isAuthenticated, message, history, userName }) {
-  const [openSnackbar, setSnackbar] = useState(false);
+function App({dispatch, isAuthenticated, message, history, userName}) {
+    const [openSnackbar, setSnackbar] = useState(false);
 
-  const routs = router.map(rout => (
-    <Route key={rout.id} path={rout.path} exact component={rout.component} />
-  ));
+    const routs = router.map(rout => (
+        <Route key={rout.id} path={rout.path} exact component={rout.component} />
+    ));
 
-  useEffect(() => {
-    dispatch(checkIfAuth(() => redirectToLogin()));
-  }, [dispatch, history]);
+    const redirectToLogin = useCallback(_ => history.replace(loginPage.path), [history]);
 
-  useEffect(() => {
-    if (message?.text) {
-      toggleSnackbar(true)();
+    useEffect(() => {
+        dispatch(checkIfAuth(() => redirectToLogin()));
+    }, [dispatch, history, redirectToLogin]);
+
+    useEffect(() => {
+        if (message?.text) {
+            toggleSnackbar(true)();
+        }
+    }, [message]);
+
+    const toggleSnackbar = state => event => {
+        setSnackbar(state);
+        if (!state) {
+            setTimeout(() => {
+                dispatch(deleteMessage());
+            }, 500);
+        }
+    };
+
+    const renderSnackbar = () => {
+        const vertical = "top",
+            horizontal = "right";
+
+        return (
+            <Snackbar
+                anchorOrigin={{vertical, horizontal}}
+                key={`${vertical},${horizontal}`}
+                open={openSnackbar}
+                autoHideDuration={2500}
+                onExited={toggleSnackbar(false)}
+                onClose={toggleSnackbar(false)}
+            >
+                <MuiAlert
+                    elevation={6}
+                    variant="filled"
+                    onClose={toggleSnackbar(false)}
+                    severity={message?.type || ""}
+                >
+                    {message?.text || ""}
+                </MuiAlert>
+            </Snackbar>
+        );
+    };
+
+    const onLogout = () => {
+        localStorage.clear();
+        dispatch(purgeApp());
+        redirectToLogin();
+    };
+
+    if (isAuthenticated) {
+        const mainPage = router.find(r => r.isMain === true);
+        return (
+            <Fragment>
+                {renderSnackbar()}
+                <Layout onLogout={onLogout} currentUser={userName}>
+                    <Suspense fallback={<div>Loading ...</div>}>
+                        <Switch>
+                            {routs}
+                            <Redirect to={mainPage.path} />
+                        </Switch>
+                    </Suspense>
+                </Layout>
+            </Fragment>
+        );
+    } else {
+        return (
+            <Fragment>
+                {renderSnackbar()}
+                <Suspense fallback={<div>Loading ...</div>}>
+                    <Switch>
+                        <Route path={loginPage.path} exact component={loginPage.component} />
+                    </Switch>
+                </Suspense>
+            </Fragment>
+        );
     }
-  }, [message]);
-
-  const toggleSnackbar = state => event => {
-    setSnackbar(state);
-    if (!state) {
-      setTimeout(() => {
-        dispatch(deleteMessage());
-      }, 500);
-    }
-  };
-
-  const redirectToLogin = () => history.replace(loginPage.path);
-
-  const renderSnackbar = () => {
-    const vertical = "top",
-      horizontal = "right";
-
-    return (
-      <Snackbar
-        anchorOrigin={{ vertical, horizontal }}
-        key={`${vertical},${horizontal}`}
-        open={openSnackbar}
-        autoHideDuration={2500}
-        onExited={toggleSnackbar(false)}
-        onClose={toggleSnackbar(false)}
-      >
-        <MuiAlert
-          elevation={6}
-          variant="filled"
-          onClose={toggleSnackbar(false)}
-          severity={message?.type || ""}
-        >
-          {message?.text || ""}
-        </MuiAlert>
-      </Snackbar>
-    );
-  };
-
-  const onLogout = () => {
-    localStorage.clear();
-    dispatch(purgeApp());
-    redirectToLogin();
-  };
-
-  if (isAuthenticated) {
-    const mainPage = router.find(r => r.isMain === true);
-    return (
-      <Fragment>
-        {renderSnackbar()}
-        <Layout onLogout={onLogout} currentUser={userName}>
-          <Suspense fallback={<div>Loading ...</div>}>
-            <Switch>
-              {routs}
-              <Redirect to={mainPage.path} />
-            </Switch>
-          </Suspense>
-        </Layout>
-      </Fragment>
-    );
-  } else {
-    return (
-      <Fragment>
-        {renderSnackbar()}
-        <Suspense fallback={<div>Loading ...</div>}>
-          <Switch>
-            <Route
-              path={loginPage.path}
-              exact
-              component={loginPage.component}
-            />
-          </Switch>
-        </Suspense>
-      </Fragment>
-    );
-  }
 }
 
 const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.authReducer.token !== null,
-    message: state.appReducer.message,
-    userName: state.authReducer.userName
-  };
+    return {
+        isAuthenticated: state.authReducer.token !== null,
+        message: state.appReducer.message,
+        userName: state.authReducer.userName
+    };
 };
 
 export default withRouter(connect(mapStateToProps)(App));
