96e0271e616f0ea5e0da9b8086b6b81e7ed0281e
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-08-07 16:35:23 +0300

    adding fields constants

---
+++ ICareClient/src/constants/fields.js
@@ -0,0 +1,9 @@
+export const DOCTOR_DETAILS_FIELDS = {
+    name: "name",
+    dateOfBirth: "dateOfBirth",
+    officialId: "officialId",
+    email: "email",
+    specialty: "specialty",
+    university: "university",
+    department: "department"
+};
