e3d4559b55d8e02ea871b0ef7301748c487b7852
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-08-21 21:21:42 +0300

    adding id to patient doctor model

--- ICareAPI/Models/PatientDoctor.cs
+++ ICareAPI/Models/PatientDoctor.cs
@@ -3,6 +3,9 @@ namespace ICareAPI.Models
 {
     public class PatientDoctor
     {
+
+        public int Id { get; set; }
+
         public int PatientId { get; set; }
 
         public int DoctorId { get; set; }
