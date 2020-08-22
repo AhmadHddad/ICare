a2bd46bcb52a54d1d367e535ecbe7f25d10ab3aa
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-08-22 13:54:15 +0300

    importing fileds names from constants

--- ICareClient/src/containers/doctors/doctorComposer/DoctorDetailsFields.js
+++ ICareClient/src/containers/doctors/doctorComposer/DoctorDetailsFields.js
@@ -1,29 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Grid} from "@material-ui/core";
-import ITextField from "components/iTextField/ITextField";
-import IDatePicker from "components/iDatePicker/IDatePicker";
+import ITextField from "common/iTextField/ITextField";
+import IDatePicker from "common/iDatePicker/IDatePicker";
 import {REGEX} from "constants/constants";
+import {DOCTOR_DETAILS_FIELDS} from "constants/fields";
 
 const propTypes = {
     control: PropTypes.any,
     errors: PropTypes.object,
-    disabled: PropTypes.object
+    disabled: PropTypes.oneOfType([PropTypes.object, PropTypes.bool])
 };
 
+const {
+    name,
+    dateOfBirth,
+    department,
+    email,
+    specialty,
+    officialId,
+    university
+} = DOCTOR_DETAILS_FIELDS;
 export default function DoctorDetailsFields({control, errors, disabled}) {
     return (
         <Grid item container direction="row" justify="center" alignItems="center" spacing={2}>
             <Grid item md={6} sm={6} xs={12}>
                 <ITextField
-                    disabled={disabled?.name}
+                    disabled={disabled?.name ?? disabled === true}
                     rules={{
                         required: true,
                         minLength: 3,
                         maxLength: 20,
                         validate: v => v.trim().length > 2
                     }}
-                    name="name"
+                    name={name}
                     control={control}
                     error={!!errors?.name}
                     id="outlined-basic"
@@ -32,8 +42,8 @@ export default function DoctorDetailsFields({control, errors, disabled}) {
             </Grid>
             <Grid item md={6} sm={6} xs={12}>
                 <IDatePicker
-                    disabled={disabled?.dateOfBirth}
-                    name="dateOfBirth"
+                    disabled={disabled?.dateOfBirth ?? disabled === true}
+                    name={dateOfBirth}
                     control={control}
                     rules={{
                         required: true
@@ -47,14 +57,14 @@ export default function DoctorDetailsFields({control, errors, disabled}) {
             </Grid>
             <Grid item md={6} sm={6} xs={12}>
                 <ITextField
-                    disabled={disabled?.officialId}
+                    disabled={disabled?.officialId ?? disabled === true}
                     rules={{
                         required: true,
                         minLength: 9,
                         maxLength: 9,
                         pattern: REGEX.number
                     }}
-                    name="officialId"
+                    name={officialId}
                     control={control}
                     error={!!errors?.officialId}
                     label={(errors?.officialId && "Official ID must be 9 digits") || "Official ID"}
@@ -62,11 +72,11 @@ export default function DoctorDetailsFields({control, errors, disabled}) {
             </Grid>
             <Grid item md={6} sm={6} xs={12}>
                 <ITextField
-                    disabled={disabled?.email}
+                    disabled={disabled?.email ?? disabled === true}
                     rules={{
                         pattern: REGEX.email
                     }}
-                    name="email"
+                    name={email}
                     control={control}
                     error={!!errors?.email}
                     label={(errors?.email && "Must be Email") || "Doctor Email (optional)"}
@@ -74,11 +84,11 @@ export default function DoctorDetailsFields({control, errors, disabled}) {
             </Grid>
             <Grid item md={6} sm={6} xs={12}>
                 <ITextField
-                    disabled={disabled?.specialty}
+                    disabled={disabled?.specialty ?? disabled === true}
                     rules={{
                         required: true
                     }}
-                    name="specialty"
+                    name={specialty}
                     control={control}
                     error={!!errors?.specialty}
                     label={(errors?.specialty && "Specialty is Required") || "Doctor Specialty"}
@@ -86,11 +96,11 @@ export default function DoctorDetailsFields({control, errors, disabled}) {
             </Grid>
             <Grid item md={6} sm={6} xs={12}>
                 <ITextField
-                    disabled={disabled?.university}
+                    disabled={disabled?.university ?? disabled === true}
                     rules={{
                         required: true
                     }}
-                    name="university"
+                    name={university}
                     control={control}
                     error={!!errors?.university}
                     label={(errors?.university && "University is Required") || "Doctor University"}
@@ -98,11 +108,11 @@ export default function DoctorDetailsFields({control, errors, disabled}) {
             </Grid>
             <Grid item md={12} sm={12} xs={12}>
                 <ITextField
-                    disabled={disabled?.department}
+                    disabled={disabled?.department ?? disabled === true}
                     rules={{
                         required: true
                     }}
-                    name="department"
+                    name={department}
                     control={control}
                     error={!!errors?.department}
                     label={(errors?.department && "Department is Required") || "Doctor Department"}
