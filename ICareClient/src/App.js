1175f8f72aa5e3e83fb61f53be893d5378f099f2
Ahmad Hddad <a7mad-7dad@hotmail.com>
2020-08-22 15:49:26 +0300

    using hooks and reorgninzing

--- ICareClient/src/App.js
+++ ICareClient/src/App.js
@@ -1,26 +1,41 @@
 import React, {Suspense, useEffect, Fragment, useState, useCallback} from "react";
-import {connect} from "react-redux";
-import {Route, Redirect, Switch, withRouter} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {Route, Redirect, Switch, useHistory} from "react-router-dom";
 
-import router, {loginPage} from "./router/routs";
-
-import Layout from "./containers/layout/Layout.js";
-
-import {checkIfAuth} from "store/actions/auth/authActions.js";
-
-import {Snackbar} from "@material-ui/core";
+// Material UI
 import MuiAlert from "@material-ui/lab/Alert";
+import {Snackbar} from "@material-ui/core";
+
+// Routs
+import router, {loginPage} from "router/routs";
+
+// Components
+import Layout from "containers/layout/Layout.js";
+
+// Actions
+import {checkIfAuth} from "store/actions/auth/authActions.js";
 import {deleteMessage, purgeApp} from "store/actions/app/appActions";
 
-function App({dispatch, isAuthenticated, message, history, userName}) {
+export default function App(props) {
+    //#region State
     const [openSnackbar, setSnackbar] = useState(false);
+    //#endregion State
 
-    const routs = router.map(rout => (
-        <Route key={rout.id} path={rout.path} exact component={rout.component} />
-    ));
+    //#region Hooks
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const {isAuthenticated, message, userName} = useSelector(state => ({
+        isAuthenticated: state.authReducer.token !== null,
+        message: state.appReducer.message,
+        userName: state.authReducer.userName
+    }));
+    //#endregion Hooks
 
     const redirectToLogin = useCallback(_ => history.replace(loginPage.path), [history]);
 
+    const toggleSnackbar = useCallback(toggleSnackbarFunc, []);
+
+    //#region LifeCycle
     useEffect(() => {
         dispatch(checkIfAuth(() => redirectToLogin()));
     }, [dispatch, history, redirectToLogin]);
@@ -29,17 +44,31 @@ function App({dispatch, isAuthenticated, message, history, userName}) {
         if (message?.text) {
             toggleSnackbar(true)();
         }
-    }, [message]);
+    }, [message, toggleSnackbar]);
 
-    const toggleSnackbar = state => event => {
-        setSnackbar(state);
-        if (!state) {
-            setTimeout(() => {
-                dispatch(deleteMessage());
-            }, 500);
-        }
-    };
+    //#endregion LifeCycle
 
+    //#region Other Functions
+    function toggleSnackbarFunc(state) {
+        return function () {
+            setSnackbar(state);
+            if (!state) {
+                setTimeout(() => {
+                    dispatch(deleteMessage());
+                }, 500);
+            }
+        };
+    }
+
+    function onLogout() {
+        localStorage.clear();
+        dispatch(purgeApp());
+        redirectToLogin();
+    }
+
+    //#endregion Other Functions
+
+    //#region Rendering Functions
     const renderSnackbar = () => {
         const vertical = "top",
             horizontal = "right";
@@ -49,7 +78,7 @@ function App({dispatch, isAuthenticated, message, history, userName}) {
                 anchorOrigin={{vertical, horizontal}}
                 key={`${vertical},${horizontal}`}
                 open={openSnackbar}
-                autoHideDuration={2500}
+                autoHideDuration={1500}
                 onExited={toggleSnackbar(false)}
                 onClose={toggleSnackbar(false)}
             >
@@ -57,19 +86,18 @@ function App({dispatch, isAuthenticated, message, history, userName}) {
                     elevation={6}
                     variant="filled"
                     onClose={toggleSnackbar(false)}
-                    severity={message?.type || ""}
+                    severity={message?.type || "info"}
                 >
                     {message?.text || ""}
                 </MuiAlert>
             </Snackbar>
         );
     };
+    //#endregion Other Functions
 
-    const onLogout = () => {
-        localStorage.clear();
-        dispatch(purgeApp());
-        redirectToLogin();
-    };
+    const routs = router.map(rout => (
+        <Route key={rout.id} path={rout.path} exact component={rout.component} />
+    ));
 
     if (isAuthenticated) {
         const mainPage = router.find(r => r.isMain === true);
@@ -99,13 +127,3 @@ function App({dispatch, isAuthenticated, message, history, userName}) {
         );
     }
 }
-
-const mapStateToProps = state => {
-    return {
-        isAuthenticated: state.authReducer.token !== null,
-        message: state.appReducer.message,
-        userName: state.authReducer.userName
-    };
-};
-
-export default withRouter(connect(mapStateToProps)(App));
