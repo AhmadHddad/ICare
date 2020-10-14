dd05648778ad29c1b58fb1521c0dbc23824b8ef3
AhmadHddad <ahmadhddad@outlook.com>
2020-10-14 22:22:01 +0300

    adding userRole

---
+++ ICareAPI/Models/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICareAPI.Models
+{
+    public class UserRole : IdentityUserRole<int>
+    {
+        public int Id { get; set; }
+
+        public User User { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
