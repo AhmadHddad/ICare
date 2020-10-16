4a44aa898bcfdb5902577b92c3acb4a0dbd9baa6
AhmadHddad <ahmadhddad@outlook.com>
2020-10-16 23:44:54 +0300

    removing unessary properties

--- ICareAPI/Models/User.cs
+++ ICareAPI/Models/User.cs
@@ -6,26 +6,19 @@ namespace ICareAPI.Models
 {
     public class User : IdentityUser<int>
     {
-        public int Id { get; set; }
+        public override string UserName { get; set; } = default!;
 
-        public string UserName { get; set; }
-
-        public string Email { get; set; }
-
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }
-
+        public override string Email { get; set; } = default!;
 
         public DateTime Created { get; set; } = DateTime.Now;
 
-        public DateTime LastAcitve { get; set; } = DateTime.Now;
+        public DateTime LastActivity { get; set; } = DateTime.Now;
 
         public bool Archived { get; set; }
 
         public DateTime? ArchivedDate { get; set; }
 
-        public ICollection<UserRole> UserRoles { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; } = default!;
 
 
     }
