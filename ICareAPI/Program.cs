100dc5a4d0ee2c8ddebeba83970d48818eda8403
AhmadHddad <ahmadhddad@outlook.com>
2020-10-16 23:42:44 +0300

    adding CreateHostBuilder for allowing ef core access to db COntext

--- ICareAPI/Program.cs
+++ ICareAPI/Program.cs
@@ -25,6 +25,13 @@ namespace ICareAPI
                         .UseStartup<Startup>();
                 }).Build();
             host.Run();
+
         }
+
+        // EF Core uses this method at design time to access the DbContext
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(
+                    webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
