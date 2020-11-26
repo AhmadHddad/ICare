23ae9a1766bea04b014f97d219be31cd7ba108b8
AhmadHddad <ahmadhddad@outlook.com>
2020-11-26 22:52:59 +0200

    adding enums

--- ICareAPI/constants/Enums.cs
+++ ICareAPI/constants/Enums.cs
@@ -1,11 +1,23 @@
+using System.Collections.Generic;
 namespace ICareAPI.constants
 {
     public static class Enums
     {
+
+
+        public static string Admin = "Admin";
+        public static string Patient = "Patient";
+        public static string Doctor = "Doctor";
+
+
+
+
         public enum EntityType
         {
-            patinet,
+            patient,
             doctor
         }
+
+
     }
 }
