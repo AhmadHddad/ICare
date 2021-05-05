e0c6bdbc8d80d41e056e273b152d7f01fb3303aa
AhmadHddad <ahmadhddad@outlook.com>
2021-05-05 19:00:35 +0300

    fixing program

--- ICareAPI/Program.cs
+++ ICareAPI/Program.cs
@@ -1,32 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ICareAPI
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
-        {
-            var host = Host.CreateDefaultBuilder(args)
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                        .UseKestrel()
-                        .UseIIS()
-                        .UseStartup<Startup>();
-                }).Build();
-            host.Run();
-
-        }
+            => CreateHostBuilder(args).Build().Run();
 
         // EF Core uses this method at design time to access the DbContext
         public static IHostBuilder CreateHostBuilder(string[] args)
