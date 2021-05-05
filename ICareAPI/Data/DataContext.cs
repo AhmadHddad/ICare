3a9667eb9cc22cf6ee31bffa14ab87759b7e0981
AhmadHddad <ahmadhddad@outlook.com>
2021-05-05 19:00:49 +0300

    fixing design time, commenting roels

--- ICareAPI/Data/DataContext.cs
+++ ICareAPI/Data/DataContext.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ICareAPI.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 
 namespace ICareAPI.Repositories
 {
-    public class DataContext : IdentityDbContext<User, Role, int,
-     IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+    public class DataContext : DbContext
     {
+
+        public DataContext()
+        {
+        }
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } = default!;
 
         public DbSet<Patient> Patients { get; set; } = default!;
 
@@ -23,16 +23,26 @@ namespace ICareAPI.Repositories
         public DbSet<PatientDoctor> PatientDoctors { get; set; } = default!;
 
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlite("Data Source=ICareDB.db");
+
+            }
+        }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<UserRole>(userRole =>
-            {
-                userRole.HasKey(ur => ur.Id);
-                userRole.HasOne(ur => ur.Role).WithMany(r => r.UserRoles).HasForeignKey(k => k.RoleId).IsRequired();
-                userRole.HasOne(ur => ur.User).WithMany(u => u.UserRoles).HasForeignKey(k => k.UserId).IsRequired();
-            });
+            // modelBuilder.Entity<UserRole>(userRole =>
+            // {
+            //     userRole.HasKey(ur => ur.Id);
+            //     userRole.HasOne(ur => ur.Role).WithMany(r => r.UserRoles).HasForeignKey(k => k.RoleId).IsRequired();
+            //     userRole.HasOne(ur => ur.User).WithMany(u => u.UserRoles).HasForeignKey(k => k.UserId).IsRequired();
+            // });
 
 
             modelBuilder.Entity<PatientDoctor>()
