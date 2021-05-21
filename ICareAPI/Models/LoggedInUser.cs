659fee8e1893eb089315350b8a0ded61ad2c2f69
AhmadHddad <ahmadhddad@outlook.com>
2021-05-21 13:31:03 +0300

    adding LoggedInUser model

---
+++ ICareAPI/Models/LoggedInUser.cs
@@ -0,0 +1,10 @@
+namespace ICareAPI.Models
+{
+    public class LoggedInUser
+    {
+
+        public string Id { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string Email { get; set; } = default!;
+    }
+}
