957d174fd7b254ea4063f93d69c6881a6f7231c6
AhmadHddad <ahmadhddad@outlook.com>
2021-05-21 13:31:31 +0300

    inilizing loggedInUser from baseController

--- ICareAPI/Controllers/BaseController.cs
+++ ICareAPI/Controllers/BaseController.cs
@@ -1,6 +1,9 @@
+using System.Security.Claims;
 using ICareAPI.Helpers;
+using ICareAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 namespace ICareAPI.Controllers
 {
@@ -10,6 +13,59 @@ namespace ICareAPI.Controllers
     [ServiceFilter(typeof(LogUserActivity))]
     public class BaseController : ControllerBase
     {
+
+        private LoggedInUser? _loggedInUser;
+
+        protected LoggedInUser? LoggedInUser
+        {
+            get
+            {
+
+                if (_loggedInUser is null)
+                {
+
+
+                    Request.Headers.TryGetValue("Authorization", out StringValues Token);
+
+
+                    if (string.IsNullOrWhiteSpace(Token))
+                    {
+                        _loggedInUser = null;
+                        return _loggedInUser;
+                    }
+                    else
+                    {
+                        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                        var userEmail = User.FindFirst(ClaimTypes.Email)?.Value;
+                        var userName = User.FindFirst(ClaimTypes.Name)?.Value;
+
+                        if (userEmail is not null && userId is not null && userName is not null)
+                        {
+                            _loggedInUser = new LoggedInUser()
+                            {
+                                Email = userEmail,
+                                Name = userName,
+                                Id = userId
+                            };
+
+                            return _loggedInUser;
+                        }
+                        else
+                        {
+                            return _loggedInUser;
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    return _loggedInUser;
+                }
+            }
+        }
+
+
 
     }
 }
