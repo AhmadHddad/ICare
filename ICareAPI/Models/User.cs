f7693157695d3b36c3cccfb4a95f8d3e493e04a5
AhmadHddad <ahmadhddad@outlook.com>
2021-06-26 22:57:45 +0300

    removing unessary user props

--- ICareAPI/Models/User.cs
+++ ICareAPI/Models/User.cs
@@ -4,18 +4,8 @@ using System.Collections.Generic;
 
 namespace ICareAPI.Models
 {
-    public class User
+    public class User : IdentityUser<int>
     {
-        public int Id { get; set; }
-
-        public string UserName { get; set; } = default!;
-
-        public string Email { get; set; } = default!;
-
-        public byte[] PasswordHash { get; set; } = default!;
-
-        public byte[] PasswordSalt { get; set; } = default!;
-
         public DateTime Created { get; set; } = DateTime.Now;
 
         public DateTime LastActivity { get; set; } = DateTime.Now;
@@ -24,7 +14,7 @@ namespace ICareAPI.Models
 
         public DateTime? ArchivedDate { get; set; }
 
-        // public ICollection<UserRole> UserRoles { get; set; } = default!;
+        public ICollection<UserRole> UserRoles { get; set; } = default!;
 
 
     }
