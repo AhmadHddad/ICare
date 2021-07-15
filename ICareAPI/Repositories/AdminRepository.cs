ad069b60af5fc7b1a9c4c5b6f9404240c97fcdf8
AhmadHddad <ahmadhddad@outlook.com>
2021-07-16 00:49:28 +0300

    adding admin repo

---
+++ ICareAPI/Repositories/AdminRepository.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using ICareAPI.Middlewares;
+using ICareAPI.Models;
+using ICareAPI.Repositories.IRepositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace ICareAPI.Repositories
+{
+    public class AdminRepository : IAdminRepository
+    {
+        private readonly DataContext _context;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminRepository(DataContext context, UserManager<AppUser> userManager,
+         RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+
+
+        }
+
+        public Task<bool> DeleteUser(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<AppUser> GetAppUser(int userId, bool includeRoles = true)
+        {
+
+
+            var user = await GetUserModelIncludeRoles(includeRoles).FirstOrDefaultAsync(user => user.Id == userId);
+
+            if (user is null)
+            {
+                throw new BadRequestException("No user with this userId");
+            }
+
+            return user;
+
+        }
+
+        public async Task<AppUser> GetAppUser(string email, bool includeRoles = true)
+        {
+            var user = await GetUserModelIncludeRoles(includeRoles).FirstOrDefaultAsync(user => user.Email == email);
+
+            if (user is null)
+            {
+                throw new BadRequestException("No user with this Email");
+            }
+
+            return user;
+
+        }
+
+        public async Task<IList<AppUser>> GetAppUsers(bool includeRoles = true) =>
+         await GetUserModelIncludeRoles(includeRoles).ToListAsync();
+
+        public Task<bool> SetUserRole(int roleId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<AppUser> UpdateUserDetails(AppUser appUser)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        private IQueryable<AppUser>? GetUserModelIncludeRoles(bool includeRoles)
+        {
+
+            var userModel = _userManager.Users;
+
+            if (includeRoles == true)
+            {
+                userModel.Include(u => u.UserRoles);
+            }
+
+            return userModel;
+        }
+    }
+}
---
+++ ICareAPI/Repositories/IRepositories/IAdminRepository.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using ICareAPI.Models;
+
+namespace ICareAPI.Repositories.IRepositories
+{
+    public interface IAdminRepository
+    {
+        Task<AppUser> GetAppUser(int userId, bool includeRoles = true);
+        Task<AppUser> GetAppUser(string email, bool includeRoles = true);
+
+        Task<IList<AppUser>> GetAppUsers(bool includeRoles = true);
+
+        Task<AppUser> UpdateUserDetails(AppUser appUser);
+
+
+        Task<bool> DeleteUser(int userId);
+
+        Task<bool> SetUserRole(int roleId);
+
+
+
+    }
+}
