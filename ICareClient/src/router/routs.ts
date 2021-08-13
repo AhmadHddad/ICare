542123ac068e0d425bf3a2c18f68553af6fc435a
AhmadHddad <AhmadHddad@outlook.com>
2021-08-13 18:06:14 +0300

    adding admin rout and enhancing routes DS

--- ICareClient/src/router/routs.ts
+++ ICareClient/src/router/routs.ts
@@ -3,6 +3,7 @@ import {lazy} from "react";
 // Icons
 import AirlineSeatFlatIcon from "@material-ui/icons/AirlineSeatFlat";
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import {ROUTES_NAME_ENUM, ROUTES_PATH_ENUM} from "constants/routesPathEnum";
 
 // Lazy Imports
@@ -11,46 +12,43 @@ const lazyPatientDetails = lazy(() => import("containers/patients/patientDetails
 const lazyLoginAndRegister = lazy(() => import("containers/login/LoginAndRegister"));
 const lazyDoctorsList = lazy(() => import("containers/doctors/doctorsList/DoctorsList"));
 const lazyDoctorDetails = lazy(() => import("containers/doctors/doctorDetails/DoctorDetails"));
+const lazyAdminConsole = lazy(() => import("containers/adminConsole/AdminConsole"));
 
 const routs = [
     {
-        id: 1,
+        name: ROUTES_NAME_ENUM.AdminConsole,
+        path: ROUTES_PATH_ENUM.AdminConsole,
+        component: lazyAdminConsole,
+        icon: SupervisorAccountIcon,
+        isMain: true
+    },
+    {
         name: ROUTES_NAME_ENUM.PatientsList,
         path: ROUTES_PATH_ENUM.PatientsList,
         component: lazyPatientsList,
         icon: AirlineSeatFlatIcon,
         isMain: true
     },
+
     {
-        id: 1,
-        name: ROUTES_NAME_ENUM.AdminConsole,
-        path: ROUTES_PATH_ENUM.AdminConsole,
-        component: lazyPatientsList,
-        icon: AirlineSeatFlatIcon,
-        isMain: true
-    },
-    {
-        id: 2,
         name: ROUTES_NAME_ENUM.PatientDetails,
         path: `${ROUTES_PATH_ENUM.PatientDetails}/:id`,
         component: lazyPatientDetails,
         hidden: true
     },
     {
-        id: 3,
         name: ROUTES_NAME_ENUM.DoctorsList,
         path: ROUTES_PATH_ENUM.DoctorsList,
         component: lazyDoctorsList,
         icon: AssignmentIndIcon
     },
     {
-        id: 4,
         name: ROUTES_NAME_ENUM.DoctorDetails,
         path: `${ROUTES_PATH_ENUM.DoctorDetails}/:id`,
         component: lazyDoctorDetails,
         hidden: true
     }
-];
+].map((rout, index) => Object.assign(rout, {id: index + 1}));
 
 export const loginPage = {
     name: ROUTES_NAME_ENUM.Login,
