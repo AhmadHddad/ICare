74092f6b6b44c806bca46a790948bd9cad5bced0
AhmadHddad <AhmadHddad@outlook.com>
2021-08-13 17:58:30 +0300

    adding ROUTES_PATH_ENUM and name

---
+++ ICareClient/src/constants/routesPathEnum.ts
@@ -0,0 +1,17 @@
+export enum ROUTES_PATH_ENUM {
+    Login = "/login",
+    PatientsList = "/patients_list",
+    AdminConsole = "/admin_console",
+    PatientDetails = "/patient_details",
+    DoctorsList = "/doctors_list",
+    DoctorDetails = "/doctor_details"
+}
+
+export enum ROUTES_NAME_ENUM {
+    Login = "Login",
+    PatientsList = "Patients List",
+    AdminConsole = "Admin Console",
+    PatientDetails = "Patient Details",
+    DoctorsList = "Doctors List",
+    DoctorDetails = "Doctor Details"
+}
