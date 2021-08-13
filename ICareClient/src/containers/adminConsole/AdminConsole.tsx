6da3479b70a61b16cbbb2e35d8d7ee2e6d788ba2
AhmadHddad <AhmadHddad@outlook.com>
2021-08-13 18:05:49 +0300

    adding admin_console component

---
+++ ICareClient/src/containers/adminConsole/AdminConsole.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface IAdminConsoleProps {}
+
+const AdminConsole: React.FunctionComponent<IAdminConsoleProps> = props => {
+    return <div>ADMIN ONLY</div>;
+};
+
+export default AdminConsole;
