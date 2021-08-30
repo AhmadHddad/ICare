c78a4fd2e2c3504e5ba9e1d76629eeef733e24eb
AhmadHddad <ahmadhddad@outlook.com>
2021-08-31 02:05:39 +0300

    adding login and register action types

--- ICareClient/src/store/actions/auth/authActionsTypes.js
+++ ICareClient/src/store/actions/auth/authActionsTypes.js
@@ -1,3 +1,9 @@
+export const LOGIN = "LOGIN";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const REGISTER = "REGISTER";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAILURE = "REGISTER_FAILURE";
 export const AUTH_START = "AUTH_START";
 export const AUTH_FAIL = "AUTH_FAIL";
 export const AUTH_SUCCESS = "AUTH_SUCCESS";
