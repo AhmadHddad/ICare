2e978dee7522e3c6240fef08f7623702a8b3f855
AhmadHddad <ahmadhddad@outlook.com>
2021-09-02 00:12:45 +0300

    adding get user and all users on admin controller

--- ICareAPI/Controllers/AdminController.cs
+++ ICareAPI/Controllers/AdminController.cs
@@ -1,8 +1,40 @@
+using System.Net;
+using System.Threading.Tasks;
+using ICareAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
 namespace ICareAPI.Controllers
 {
     public class AdminController : BaseController
     {
-        
-        
+        private readonly IAdminRepository _adminRepository;
+
+        public AdminController(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+
+        }
+
+        [HttpGet("GetUsers")]
+        public async Task<IActionResult> GetAllUsers(bool includeRoles)
+        {
+
+            var users = await _adminRepository.GetAppUsers(includeRoles);
+
+
+            return Ok(users);
+        }
+
+
+        [HttpGet("GetUserById")]
+        public async Task<IActionResult> GetUserById(int userId, bool includeRoles)
+        {
+
+            var user = await _adminRepository.GetAppUser(userId, includeRoles);
+
+
+            return Ok(user);
+        }
+
     }
 }
