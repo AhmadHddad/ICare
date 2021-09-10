167ea4adb8736c158eae2c2c2ea1bf0cf9a6a4ba
AhmadHddad <AhmadHddad@outlook.com>
2021-09-10 23:32:52 +0300

    relocating token service

--- ICareAPI/Extensions/IdentityServiceExtension.cs
+++ ICareAPI/Extensions/IdentityServiceExtension.cs
@@ -1,13 +1,18 @@
+using System.Text;
 using ICareAPI.Models;
 using ICareAPI.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using ICareAPI.constants;
 
 namespace ICareAPI.Extensions
 {
     public static class IdentityServiceExtensions
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services)
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration _config)
         {
 
             // Configuration for Identity framework
@@ -17,8 +22,9 @@ namespace ICareAPI.Extensions
                 opt.Password.RequireDigit = false;
                 opt.Password.RequiredLength = 4;
                 opt.Password.RequireNonAlphanumeric = false;
-                opt.Lockout.AllowedForNewUsers =false;
+                opt.Lockout.AllowedForNewUsers = false;
                 opt.Password.RequireUppercase = false;
+                opt.Password.RequireLowercase = false;
 
             }).AddRoles<AppRole>()
             .AddRoleManager<RoleManager<AppRole>>()
@@ -26,6 +32,29 @@ namespace ICareAPI.Extensions
             .AddRoleValidator<RoleValidator<AppRole>>()
             .AddEntityFrameworkStores<DataContext>();
 
+
+
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+
+                };
+            });
+
+
+            services.AddAuthorization(opt =>
+                {
+                    opt.AddPolicy("REQUIRE_ADMIN_ROLE", policy => policy.RequireRole(RolesEnum.Admin.ToString()));
+                    // opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+                });
 
 
             return services;
